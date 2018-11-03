bac5db69c4132b0a0ac1e13a7277218987a49eed
Brandon Fletcher <brandonjfletcher@gmail.com>
2018-11-02 19:36:42 -0500

    Now Loads Google MapsActivity...

--- app/src/main/java/com/example/brandon/myfoodtruckapplication/UserDashboard.java
+++ app/src/main/java/com/example/brandon/myfoodtruckapplication/UserDashboard.java
@@ -5,23 +5,39 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class UserDashboard extends AppCompatActivity implements View.OnClickListener {
+
+    // [START declare_auth]
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_dashboard);
+
+        /* Buttons */
+        findViewById(R.id.button7).setOnClickListener(this);
+        findViewById(R.id.button8).setOnClickListener(this);
+        findViewById(R.id.button10).setOnClickListener(this);
+
+
+        // [START initialize_auth]
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
     }
 
     @Override
     public void onClick(View v) {
         int i = v.getId();
         if (i == R.id.button7) {
-            startActivity(new Intent(this, UserLogin.class));
+            startActivity(new Intent(this, MapsActivity.class));
         } else if (i == R.id.button8) {
             // Review
         } else if (i == R.id.button10) {
-            // Sign Out and Close
+            mAuth.signOut();
+            startActivity(new Intent(this, MainActivity.class));
         }
     }
 }
